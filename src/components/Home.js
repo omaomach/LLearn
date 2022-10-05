7d38c8f65841150055c3ff5a48336f099d16d1fd
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-05 03:53:03 +0300

    added rotating world gif to Home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import rotatingGif from "../assets/rotating2.gif"
+
+function Home() {
+
+    return (
+        <div>
+            <img src={rotatingGif} alt="world"/>
+        </div>
+    )
+
+}
+
+export default Home
